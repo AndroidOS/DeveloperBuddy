3027a7526ed9599da92afc7a16aa8755a9fe35c3
AndroidOS <burrowsgit@gmail.com>
2019-12-25 15:48:37 +1100

    Added retrofit to Room Model conversion

--- app/src/main/java/com/example/developerbuddy/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/developerbuddy/viewmodel/ListViewModel.kt
@@ -20,11 +20,13 @@ class ListViewModel(application: Application) : BaseViewModel(application) {
     private val disposable = CompositeDisposable()
 
     val soItems = MutableLiveData<List<Item>>()
+    val stackItems = MutableLiveData<List<Stack>>()
     val loading = MutableLiveData<Boolean>()
     val menu_email = MutableLiveData<Boolean>()
 
     fun refresh() {
         fetchFromRemote()
+        
     }
 
     
@@ -37,13 +39,25 @@ class ListViewModel(application: Application) : BaseViewModel(application) {
                 .subscribeWith(object : DisposableSingleObserver<RootObject>() {
                     override fun onSuccess(itemList: RootObject) {
                         //Log.d(TAG, "List size =  ${jobList.size}")
-                        Toast.makeText(
-                            getApplication(),
-                            "Items retrieved from endpoint",
-                            Toast.LENGTH_SHORT
-                        ).show()
+//                        Toast.makeText(
+//                            getApplication(),
+//                            "Items retrieved from endpoint",
+//                            Toast.LENGTH_SHORT
+//                        ).show()
+
                         soItems.value = itemList.items
-                        //storeItemsLocally(itemList)
+                        if (itemList.items != null) {
+                            storeItemsLocally(createStackListFromDownload(itemList.items))
+                            //fetchFromDatabase()
+                        } else {
+                            Toast.makeText(
+                                getApplication(),
+                                "Error retrieving items",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+                        //fetchFromDatabase()
 
                     }
 
@@ -57,18 +71,65 @@ class ListViewModel(application: Application) : BaseViewModel(application) {
         )
     }
 
+    private fun fetchFromDatabase() {
+        loading.value = true
+        launch {
+            val stacks = StackDatabase(getApplication()).stackDao().getAllStacks()
+            stacksRetrieved(stacks)
+            //Toast.makeText(getApplication(), "Stack users retrieved from database. ${stacks.size} objects", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun stacksRetrieved(stackList: List<Stack>) {
+        stackItems.value = stackList
+        loading.value = false
+        Toast.makeText(
+            getApplication(),
+            "Database retrieval ${stackList.size} objects",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
     private fun storeItemsLocally(list: List<Stack>) {
         launch {
             val dao = StackDatabase(getApplication()).stackDao()
             dao.deleteAllStacks()
             val result = dao.insertAll(*list.toTypedArray())
             var i = 0
+            Toast.makeText(
+                getApplication(),
+                "Database insert ${result.size} objects",
+                Toast.LENGTH_SHORT
+            ).show()
             while (i < list.size) {
                 list[i].uuid = result[i].toInt()
                 ++i
             }
 
+            fetchFromDatabase()
+
         }
+    }
+
+    private fun createStackListFromDownload(list: List<Item>): List<Stack> {
+        var stackList = mutableListOf<Stack>()
+        for (item in list) {
+            val stack = Stack(
+                item.user?.user_id,
+                item.user?.reputation,
+                item.user?.user_type,
+                item.user?.profile_image,
+                item.user?.display_name,
+                item.name,
+                item.user?.accept_rate,
+                item.badge_type,
+                item.badge_id
+            )
+
+            stackList.add(stack)
+        }
+
+        return stackList
     }
 
 
